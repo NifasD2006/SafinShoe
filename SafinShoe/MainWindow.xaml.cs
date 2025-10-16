3a6c52094661c51017f6ef73a500492f3ac27383
Danis <danis.safin.99@list.ru>
2025-10-16 09:13:18 +0500

    Добавьте файлы проекта.

---
+++ SafinShoe/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SafinShoe
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new ShoesPage());
+            Manager.MainFraim = MainFrame;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFraim.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BackBtn.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BackBtn.Visibility = Visibility.Hidden;
+            }
+
+
+
+        }
+    }
+}
---
+++ SafinShoe/ShoesPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SafinShoe
+{
+    /// <summary>
+    /// Логика взаимодействия для ShoesPage.xaml
+    /// </summary>
+    public partial class ShoesPage : Page
+    {
+        public ShoesPage()
+        {
+            InitializeComponent();
+            var currentShoes = SafinShoeEntities.GetContext().Products.ToList();
+            ShoeListView.ItemsSource = currentShoes;
+        }
+    }
+}
